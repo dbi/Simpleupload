90172feb8f58fefd123a888e0634897f35c7c15d
Johan Rylander <johan@rylander.cc>
2010-11-03 11:43:46 +0100

    File names as 0 - 19

--- app.rb
+++ app.rb
@@ -2,21 +2,36 @@ require 'rubygems'
 require 'sinatra'
 require 'fileutils'
 require 'json'
-
-# upload with:
-# curl -v -F "data=@/path/to/filename"  http://localhost:9292/upload
+require 'thread'
 
 get '/' do
   content_type :json
   { :files => Dir.glob('files/**/*.*') }.to_json
 end
 
+# upload with:
+# curl -v -F "data=@/path/to/filename"  http://localhost:4567/user/filename
+
 post '/upload' do
   userdir = File.join("files")
   FileUtils.mkdir_p(userdir)
-  filename = File.join(userdir, "#{rand(32**8).to_s(32)}-#{params[:data][:filename]}")
+  filename = File.join(userdir, "#{getNextFilename}.jpg")
   
+  datafile = params[:data]
+
   File.open(filename, 'wb') do |file|
-    file.write(params[:data][:tempfile].read)
+    file.write(datafile[:tempfile].read)
   end
 end
+
+@@semaphore = Mutex.new
+@@nextFileIndex = -1
+def getNextFilename
+  @@semaphore.synchronize {
+    @@nextFileIndex = ((@@nextFileIndex + 1) % 20)
+  }
+end
+
+# max 20
+# äldst föst
+# tidsstämpel
